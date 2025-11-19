c9cda546e66e65d903c7bb584997055150a682b4
lonie <12333holbertonstudents.com>
2025-11-19 05:26:00 +0100

    Write a function that prints the 9 times table starting with 0 (Betty compliant)

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * times_table - Prints the 9 times table starting with 0
+ */
+void times_table(void)
+{
+	int i, j, p;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			p = i * j;
+			if (j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (p < 10)
+					_putchar(' ');
+			}
+			if (p >= 10)
+				_putchar(p / 10 + '0');
+			else if (j != 0)
+				_putchar(' ');
+			_putchar(p % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
