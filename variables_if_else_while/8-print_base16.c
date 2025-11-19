f65096c7a8285062deee6a80ab245b8573d5797b
lonie <12333holbertonstudents.com>
2025-11-19 04:42:05 +0100

    Write a program that prints all the numbers of base 16 in lowercase using putchar (Betty compliant)

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(void)
+{
+	int digit;
+	char letter;
+
+	for (digit = 0; digit < 10; digit++)
+	{
+		putchar(digit + '0');
+	}
+	for (letter = 'a'; letter <= 'f'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+
+	return (0);
+}
