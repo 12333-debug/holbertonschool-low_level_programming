b03f6c957b8c5d1f4f63383f1145377117e798dc
lonie <12333holbertonstudents.com>
2025-11-19 06:36:44 +0100

    Implement reset_to_98 function to update value via pointer (Betty compliant)

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -10,5 +10,6 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 int _putchar(char c);
 void print_diagsums(int *a, int size);
+void reset_to_98(int *n);
 
 #endif /* MAIN_H */
