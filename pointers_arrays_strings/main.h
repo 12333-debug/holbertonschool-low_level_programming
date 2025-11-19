27879111941a8eef7bd2b1db4e9bce90ddea8ed2
lonie <12333holbertonstudents.com>
2025-11-19 06:39:16 +0100

    Implement swap_int function to swap two integers via pointers (Betty compliant)

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -11,5 +11,6 @@ void print_chessboard(char (*a)[8]);
 int _putchar(char c);
 void print_diagsums(int *a, int size);
 void reset_to_98(int *n);
+void swap_int(int *a, int *b);
 
 #endif /* MAIN_H */
