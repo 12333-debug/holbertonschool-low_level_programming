dd8d87477d5de46d49e999fd8988c46b503b1436
lonie <12333holbertonstudents.com>
2025-11-19 04:35:17 +0100

    Write a program that prints the alphabet in lowercase, then in uppercase (Betty compliant)

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(void)
+{
+	char lower;
+	char upper;
+
+	for (lower = 'a'; lower <= 'z'; lower++)
+	{
+		putchar(lower);
+	}
+	for (upper = 'A'; upper <= 'Z'; upper++)
+	{
+		putchar(upper);
+	}
+	putchar('\n');
+
+	return (0);
+}
