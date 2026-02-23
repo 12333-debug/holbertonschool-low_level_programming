726e79d493114941316b9ed5fb4bb15ebcaef3c5
12333-debug <12333@holbertonstudents.com>
2026-02-23 15:12:23 +0100

    Write a C program that prints the size of various types on the computer it is compiled and run on.

---
+++ hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Write a C program that prints the size of various types on the computer it is compiled and run on.
+ *
+ * Return : always 0
+ */
+int main(void)
+{
+	printf(sizeof(char));
+	printf(sizeof(int));
+	printf(sizeof(long));
+	printf(sizeof(float));
+
+	return (0);
+}
