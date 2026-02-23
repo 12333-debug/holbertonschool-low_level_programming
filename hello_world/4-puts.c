588647c1ccc9a075aa1a02e5287b6cf0dd2ec4ef
12333-debug <12333@holbertonstudents.com>
2026-02-23 14:35:47 +0100

    Write a C program that prints exactly, followed by a new line.

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 /**
  * main - Write a C program
- * Return: toujours 0
- */ 
+ *
+ * Return: always 0
+ */
+
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle\n");
-	return 0;
+	return (0);
 }
