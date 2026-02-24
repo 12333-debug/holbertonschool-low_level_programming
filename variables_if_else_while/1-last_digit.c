efaae1c56222363acfd256998c54f98d613102d5
12333-debug <12333@holbertonstudents.com>
2026-02-24 13:57:01 +0100

    This program will assign a random number to the variable

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * more headers goes there
+ *
+ * betty style doc for function main goes there
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int n;
+	int Last_digit_of;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	Last_digit_of = n % 10;
+
+	if (Last_digit_of > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, Last_digit_of);
+	if (Last_digit_of == 0)
+		printf("Last digit of %d is %d and is 0\n", n, Last_digit_of);
+	if (Last_digit_of < 6 && Last_digit_of != 0)
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, Last_digit_of);
+	return (0);
+}
