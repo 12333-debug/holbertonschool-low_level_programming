27a566231c3a73b07d703aa9c1608e94fe157e5a
12333-debug <12333@holbertonstudents.com>
2026-03-07 16:01:42 +0100

    change the code

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,23 +1,29 @@
 #include "main.h"
 
+/**
+ * rev_string - reverses a string
+ * @s: string to reverse
+ *
+ * Return: void
+ */
 void rev_string(char *s)
 {
-    char *j = s;
-    char tmp;
+	int i = 0;
+	int j = 0;
+	char temp;
 
-    while (*s)
-    {
-        s++;
-    }
-        s--;
-    while (s>j)
-    {
-        
-        tmp = *s;
-        *s = *j;
-        *j = tmp;
+	while (s[j] != '\0')
+		j++;
 
-    j++;
-    s--;
-    }    
+	j--;
+
+	while (i < j)
+	{
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
+
+		i++;
+		j--;
+	}
 }
