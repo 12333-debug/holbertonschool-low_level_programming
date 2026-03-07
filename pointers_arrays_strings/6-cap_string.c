3fac166674df562a68bb224de72211cb76e06db3
12333-debug <12333@holbertonstudents.com>
2026-03-07 16:46:13 +0100

    add the code and the comment

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @s: string to modify
+ *
+ * Return: pointer to s
+ */
+char *cap_string(char *s)
+{
+	int i = 0;
+	char sep[] = " \t\n,;.!?\"(){}";
+	int j;
+
+	if (s[0] >= 'a' && s[0] <= 'z')
+		s[0] = s[0] - 32;
+
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (sep[j] != '\0')
+		{
+			if (s[i] == sep[j] && s[i + 1] >= 'a' && s[i + 1] <= 'z')
+			{
+				s[i + 1] = s[i + 1] - 32;
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+
+	return (s);
+}
