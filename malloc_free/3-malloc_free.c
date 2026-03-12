775cd8844507f8ec4853c246ef1c00bbf356c53a
12333-debug <12333@holbertonstudents.com>
2026-03-12 13:15:14 +0100

    Add alloc grid script

---
+++ malloc_free/3-malloc_free.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int **alloc_grid(int width, int height)
+
+{
+int i;
+int j;
+int grid;
+
+
+if (width <= 0 || height <= 0)
+return (NULL);
+
+grid = malloc(height * sizeof(int *));
+if (grid == NULL)
+return (NULL);
+
+for (i = 0; i < height ; i++)
+{
+grid[i] = malloc(width * sizeof(int *));
+}
+if (grid[i] == NULL)
+for (j = 0; j < i; i++)
+{
+free(grid[j]);
+free grid
+return ();
+}
+{
+for (i = 0; i < height; i++)
+for (j = 0; j < width ; j++)
+
+grid [i][j] = 0
+
+retunr grid
+}
