3cf63f02cfe55744e7cfffa9add836d045d32caf
lonie <12333@holbertonstudents.com>
2026-04-25 18:22:21 +0200

    add betty style

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,11 +1,7 @@
 #ifndef FUNCTION_POINTERS_H
-#define UNCTION_POINTERS_H
-#include <stddef.h>
-
+#define FUNCTION_POINTERS_H
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-
-
 #endif
